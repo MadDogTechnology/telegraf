4e3aae3a38c3b92b1bbdcffa07bd7c6148a03d5d
Shawn <syang@maddogtechnology.com>
2019-04-18 16:40:09 -0400

    RP-5208

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,9 @@ if [ "${1:0:1}" = '-' ]; then
     set -- telegraf "$@"
 fi
 
+export METRIC_BATCH_SIZE=${METRIC_BATCH_SIZE:-5000}
+export METRIC_BUFFER_LIMIT=${METRIC_BUFFER_LIMIT:-1000000}
+
 envsubst < /telegraf-template.conf > /etc/telegraf/telegraf.conf
 
 exec "$@"
